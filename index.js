ab042302fa440e5a858afd0795c4f351c9307819
Amber Thomas <ProQuestionAsker@gmail.com>
2020-08-27 12:09:02 -0700

    switching ids

--- index.js
+++ index.js
@@ -344,7 +344,7 @@ async function runBatches(){
 	// run the script in batches
 
 	try {
-		for (let i = 0; i < 2; i += 1){
+		for (let i = 10; i < 13; i += 1){
 			const finished = webcams.slice(i, i + 2).map(async cam =>  makeZoo(cam))
 			console.log(finished)
 
