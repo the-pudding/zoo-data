ffb1eb0a29667ecc31dcde746f25e795a2241494
Amber Thomas <ProQuestionAsker@gmail.com>
2020-09-07 11:11:23 -0700

    adding ffmpeg to fix video play

--- index.js
+++ index.js
@@ -364,7 +364,7 @@ async function runBatches(){
 	// run the script in batches
 
 	try {
-		for (let i = 10; i < 20; i += 1){
+		for (let i = 0; i < 5; i += 1){
 			const finished = webcams.slice(i, i + 1).map(async cam =>  makeZoo(cam))
 			console.log(finished)
 
