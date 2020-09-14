94fc66bf59c6cab8b3e5e3c73dbb80589ec76b3e
Amber Thomas <ProQuestionAsker@gmail.com>
2020-09-14 11:45:30 -0700

    different cams

--- index.js
+++ index.js
@@ -298,7 +298,7 @@ async function runBatches(){
 	// run the script in batches
 
 	try {
-		for (let i = 0; i < 5; i += 1){
+		for (let i = 5; i < 15; i += 1){
 			
 			const finished = webcams.slice(i, i + 1).map(async cam =>  makeZoo(cam))
 
